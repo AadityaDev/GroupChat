16422ecd2d272f15a89a094a3f732563ab1cc990
AadityaDev <adityaverma.dev@gmail.com>
2016-05-22 09:15:21 +0530

    First commit

---
+++ app/src/main/java/com/technawabs/groupchat/UserInfo.java
@@ -0,0 +1,50 @@
+package com.technawabs.groupchat;
+
+public class UserInfo {
+
+    String body;
+    String userName;
+    String name;
+    String imageUrl;
+    String messageTime;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(String messageTime) {
+        this.messageTime = messageTime;
+    }
+}
